dbd819089db16e6aee211c6425182534e473d71c
nevsiruj <98376961+nevsiruj@users.noreply.github.com>
2023-04-30 00:07:27 -0300

    2 files modified

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,32 +1,56 @@
 <template>
-  <footer class="bg-white text-gray-800 py-4">
-    <div class="container mx-auto flex justify-between items-center">
-      <div>
-        <h4 class="text-xl font-bold">Gonzalo Suárez</h4>
-        <p class="text-gray-500">Desarrollador web</p>
+  <footer class="bg-blue-100 text-gray-800 py-4">
+    <div
+      class="
+        container
+        mx-auto
+        flex flex-col
+        md:flex-row
+        justify-between
+        items-center
+      "
+    >
+      <div class="text-center mb-4 md:mb-0">
+        <h4 class="text-2xl font-semibold text-blue-500">La Previa</h4>
+        <p class="text-gray-500">
+          <a
+            href="https://wa.me/3544444428"
+            target="_blank"
+            rel="noopener noreferrer"
+            class="
+              inline-flex
+              items-center
+              px-4
+              py-2
+              text-gray-900
+              bg-green-200
+              rounded
+              hover:bg-green-300
+            "
+          >
+            <i class="fab fa-whatsapp mr-1"></i>
+            Chat WhatsApp
+          </a>
+        </p>
       </div>
-      <div class="flex justify-center items-center">
-        <a
-          href="https://twitter.com/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="mx-2"
-          ><i class="fab fa-twitter fa-lg"></i
-        ></a>
+
+      <div class="flex justify-center items-center space-x-4">
         <a
           href="https://www.instagram.com/"
           target="_blank"
           rel="noopener noreferrer"
-          class="mx-2"
-          ><i class="fab fa-instagram fa-lg"></i
-        ></a>
+          class="text-blue-400 hover:text-blue-700"
+        >
+          <i class="fab fa-instagram fa-lg"></i>
+        </a>
         <a
-          href="https://www.linkedin.com/"
+          href="https://www.facebook.com/"
           target="_blank"
           rel="noopener noreferrer"
-          class="mx-2"
-          ><i class="fab fa-linkedin fa-lg"></i
-        ></a>
+          class="text-blue-400 hover:text-blue-700"
+        >
+          <i class="fab fa-facebook fa-lg"></i>
+        </a>
       </div>
     </div>
   </footer>
@@ -35,7 +59,15 @@
 export default {};
 </script>
 <style scoped>
+/* @import 'https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.2.16/tailwind.min.css'; */
+footer {
+  background-color: #f1f8ff;
+}
+footer h4 {
+  color: #007bff;
+}
+
 footer a:hover {
-  color: #48bb78;
+  color: #007bff;
 }
 </style>
--- src/components/Services.vue
+++ src/components/Services.vue
@@ -1,41 +1,64 @@
 <template>
-  <section id="best-sellers" class="my-16">
-    <h2 class="text-2xl font-bold text-center mb-8 text-gray-900">
-      🔥 Más vendidos
-    </h2>
+  <section id="store" class="my-16">
+    <h2 class="text-2xl font-bold text-center mb-8 text-blue-500">🛍️ Tienda</h2>
     <div class="container mx-auto">
-      <div class="grid grid-cols-1 sm:grid-cols-2 gap-4">
-        <div class="bg-white p-8 rounded-lg shadow-lg">
-          <img
-            src="https://via.placeholder.com/300x300"
-            alt="Product 1"
-            class="w-full h-64 object-cover rounded-lg"
-          />
-          <h3 class="text-lg font-bold mt-4 text-blue-500">Producto 1</h3>
+      <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+        <div class="p-4">
+          <div class="bg-white rounded-lg shadow-lg">
+            <img
+              src="https://via.placeholder.com/300x300"
+              alt="Product 1"
+              class="w-full h-48 object-cover rounded-lg mx-auto mb-4"
+            />
+            <h3 class="text-lg font-bold mt-4 text-center text-blue-500">
+              Producto 1
+            </h3>
+            <a
+              href="https://wa.me/5550123456789"
+              class="
+                mt-4
+                block
+                bg-green-500
+                text-white
+                font-bold
+                py-2
+                px-4
+                rounded-full
+                shadow-md
+              "
+            >
+              <i class="fab fa-whatsapp mr-2"></i> Contactar
+            </a>
+          </div>
         </div>
-        <div class="bg-white p-8 rounded-lg shadow-lg">
-          <img
-            src="https://via.placeholder.com/300x300"
-            alt="Product 2"
-            class="w-full h-64 object-cover rounded-lg"
-          />
-          <h3 class="text-lg font-bold mt-4 text-blue-500">Producto 2</h3>
-        </div>
-        <div class="bg-white p-8 rounded-lg shadow-lg">
-          <img
-            src="https://via.placeholder.com/300x300"
-            alt="Product 3"
-            class="w-full h-64 object-cover rounded-lg"
-          />
-          <h3 class="text-lg font-bold mt-4 text-blue-500">Producto 3</h3>
-        </div>
-        <div class="bg-white p-8 rounded-lg shadow-lg">
-          <img
-            src="https://via.placeholder.com/300x300"
-            alt="Product 4"
-            class="w-full h-64 object-cover rounded-lg"
-          />
-          <h3 class="text-lg font-bold mt-4 text-blue-500">Producto 4</h3>
+
+        <div class="p-4">
+          <div class="bg-white rounded-lg shadow-lg">
+            <img
+              src="https://via.placeholder.com/300x300"
+              alt="Product 2"
+              class="w-full h-48 object-cover rounded-lg mx-auto mb-4"
+            />
+            <h3 class="text-lg font-bold mt-4 text-center text-blue-500">
+              Producto 2
+            </h3>
+            <a
+              href="https://wa.me/5550123456789"
+              class="
+                mt-4
+                block
+                bg-green-500
+                text-white
+                font-bold
+                py-2
+                px-4
+                rounded-full
+                shadow-md
+              "
+            >
+              <i class="fab fa-whatsapp mr-2"></i> Contactar
+            </a>
+          </div>
         </div>
       </div>
     </div>
